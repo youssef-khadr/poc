1a707b68ba7fbcb6ebcc5571d3d4d1641d7d4ff2
youssef-khadr <63048848+youssef-khadr@users.noreply.github.com>
2020-07-10 20:48:36 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 provider "aws" {
-  profile    = "default"
   region     = "us-east-1"
 }
 data "aws_caller_identity" "current" {
 }
 data "aws_region" "current" {
 
+}
+terraform {
+    backend "s3" {
+      encrypt = true
+      bucket = "ifbi-poc-terraform"
+      key = "path/path/terraform.tfstate"
+      region = "${data.aws_region.current.name}"
+  }
 }
 output "awsRegion" {
   value = "${data.aws_region.current.name}"
