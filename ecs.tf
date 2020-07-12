3f545c06a2db70e6613597b07ce094075d980b5d
youssef-khadr <63048848+youssef-khadr@users.noreply.github.com>
2020-07-12 02:20:20 +0200

    Add files via upload

--- ecs.tf
+++ ecs.tf
@@ -9,29 +9,29 @@ data "aws_iam_policy_document" "ecs_tasks_execution_role" {
   }
 }
 
-resource "aws_iam_role" "ecs_tasks_execution_role" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.ecs_tasks_execution_role.json}"
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
-  role       = "${aws_iam_role.ecs_tasks_execution_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_ecs_task_definition" "ecstaskDefinition" {
-  family                = "pocFamily"
-  container_definitions = file("taskdef.json")
-  execution_role_arn = "${aws_iam_role.ecs_tasks_execution_role.arn}"
-  requires_compatibilities = ["FARGATE"]
-  network_mode = "awsvpc"
-  cpu = 10
-  memory = 128
-}
+#resource "aws_iam_role" "ecs_tasks_execution_role" {
+#  name               = "ecsTaskExecutionRole"
+#  assume_role_policy = "${data.aws_iam_policy_document.ecs_tasks_execution_role.json}"
+#}
+#
+#resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
+#  role       = "${aws_iam_role.ecs_tasks_execution_role.name}"
+#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#}
+#
+#resource "aws_ecs_task_definition" "ecstaskDefinition" {
+#  family                = "pocFamily"
+#  container_definitions = file("taskdef.json")
+#  execution_role_arn = "${aws_iam_role.ecs_tasks_execution_role.arn}"
+#  requires_compatibilities = ["FARGATE"]
+#  network_mode = "awsvpc"
+#  cpu = 10
+#  memory = 128
+#}
 resource "aws_ecs_service" "ecsService" {
   name            = "pocService"
   cluster         = " arn:aws:ecs:us-east-1:854889429362:cluster/pocCluster"
-  task_definition = "${aws_ecs_task_definition.ecstaskDefinition.arn}"
+  task_definition = "pocFamily"
   desired_count   = 1
   launch_type = "FARGATE"
 
